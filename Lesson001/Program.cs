29663080333ec3702a8e9975b91cb7a1b92099d7
«Dane» <slip_dane@mail.ru>
2022-09-15 20:57:00 +0300

    Add Lesson001  1

--- Lesson001/Program.cs
+++ Lesson001/Program.cs
@@ -6,5 +6,7 @@
 -3 -> 9 
 -7 -> 49
 */
-
-int userNumber = 
+Console.WriteLine("Введите значение");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int result = userNumber * userNumber;
+Console.WriteLine($"Квадрат числа {userNumber} равен {result}");
