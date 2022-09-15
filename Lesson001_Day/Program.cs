81a780bfa4d0f4f16af04fa531879a272c89a872
«Dane» <slip_dane@mail.ru>
2022-09-15 21:54:00 +0300

    Add lesson001 weekday

---
+++ Lesson001_Day/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите значение дня недели от 1 до 7: ");
+int userNumber1 = Convert.ToInt32(Console.ReadLine());
+
+if (userNumber1 == 1)
+Console.WriteLine("День недели - Понедельник");
+else
+    if (userNumber1 == 2)
+        Console.WriteLine("День недели - Вторник");
+    else
+        if (userNumber1 == 3)
+            Console.WriteLine("День недели - Среда");
+        else
+            if (userNumber1 == 4)
+                Console.WriteLine("День недели - Четверг");
+            else
+
+                if (userNumber1 == 5)
+                    Console.WriteLine("День недели - Пятница");
+                else
+                    if (userNumber1 == 6)
+                        Console.WriteLine("День недели - Суббота");
+                    else
+                        if (userNumber1 == 7)
+                            Console.WriteLine("День недели - Воскресенье");
+                        else
+                            Console.WriteLine("Неверный номер");
