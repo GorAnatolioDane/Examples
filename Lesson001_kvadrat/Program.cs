6ae0a989cb333eaabe4576fff0762c81a658ac49
«Dane» <slip_dane@mail.ru>
2022-09-15 21:09:19 +0300

    Add lesson001 kvadrat

---
+++ Lesson001_kvadrat/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите значение 1: ");
+int userNumber1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение 2: ");
+int userNumber2 = Convert.ToInt32(Console.ReadLine());
+
+if (userNumber1 == userNumber2 * userNumber2)
+{
+    Console.WriteLine("Является ли первое число - квадратом второго числа?");
+    Console.WriteLine("Да!");
+}
+else
+{
+   Console.WriteLine("Является ли первое число - квадратом второго числа?");
+    Console.WriteLine("Нет") ;
+}
