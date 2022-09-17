01b6eccd02a2d22992a09e40ff5f90c59f900c55
«Dane» <slip_dane@mail.ru>
2022-09-18 00:40:19 +0300

    Add lesson001_HomeWork01

---
+++ Lesson001_HomeWork01/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите первое число: ");
+int userNumber1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int userNumber2 = Convert.ToInt32(Console.ReadLine());
+
+if (userNumber1 > userNumber2)
+    {
+        Console.WriteLine("Наибольшим числом является: ");
+        Console.WriteLine(userNumber1);
+        Console.WriteLine("Наименьшим числом является: ");
+        Console.WriteLine(userNumber2);
+    }
+    else if (userNumber1 < userNumber2)
+    {
+        Console.WriteLine("Наибольшим числом является: ");
+        Console.WriteLine(userNumber2);
+        Console.WriteLine("Наименьшим числом является: ");
+        Console.WriteLine(userNumber1);
+}
+
+else if (userNumber1 == userNumber2)
+    {
+        Console.WriteLine("Числа равны! ");
+    }
