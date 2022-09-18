ff6da759a0d3e967543d0ba7267fa6fdff9a811d
«Dane» <slip_dane@mail.ru>
2022-09-18 22:54:15 +0300

    Add Lesson001_HomeWork04

---
+++ Lesson001_HomeWork04/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int count = 0 ;
+while (count < userNumber - 1)
+{
+    count += 2;
+    Console.Write(count + " ");
+}
